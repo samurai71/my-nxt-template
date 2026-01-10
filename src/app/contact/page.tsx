1dc14e657d90b63113a578509cd19914aa447e6b
Mark Landeryou <samurai96@hotmail.com>
2026-01-10 16:11:49 -0500

    Develop (#4)
    * feat: :sparkles: add Footer component and integrate it into RootLayout
    
    * feat: :sparkles: add Privacy Policy page, enhance Footer with links, and update global styles for headings and paragraphs
    
    * feat: :sparkles: update metadata structure for RootLayout, Home, and Privacy pages; add Terms and Conditions page
    
    * feat: :sparkles: remove unused SVG files from public directory
    
    * feat: :sparkles: enhance layout and styling for Terms and Conditions page; add dark mode support
    
    * feat: :sparkles: update Prettier configuration; add new settings and remove deprecated config
    
    * feat: :sparkles: refactor layout and header components; improve structure and styling
    
    * refactor: standardize string quotes to single quotes and improve code readability
    
    - Updated all instances of double quotes to single quotes in various files for consistency.
    - Enhanced the formatting and structure of the Privacy Policy and Terms and Conditions pages for better readability.
    - Improved the ThemeToggle and Carousel components by refining the theme management logic and ensuring compatibility with global objects.
    - Refactored InputGroup components to streamline props handling and improve accessibility.
    
    * feat: :sparkles: add About and Contact pages with metadata and layout
    
    * refactor: standardize navigation link styles to uppercase
    
    * feat: :sparkles: implement dynamic link classes for navigation based on current pathname
    
    * feat: :sparkles: update activelink to italic
    
    * chore: :recycle: updated the title of the page
    
    * refactor: :recycle: fixed code with prettier
    
    * chore: :arrow_up: updated packages in package dot json
    
    * updated prettierignore file
    
    * feat: :sparkles: added a contact form
     in contact but commented it out

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 
+// import MyForm from '@/components/contactForm'
+
 export const metadata: Metadata = {
   title: 'Contact',
 }
@@ -8,6 +10,7 @@ export default function Contact() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <div className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
         <h2>Contact</h2>
+        {/* <MyForm /> */}
       </div>
     </div>
   )
