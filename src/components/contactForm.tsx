da5808f9a695840df5dbc16123166ecf432bf21b
Mark Landeryou <samurai96@hotmail.com>
2026-01-23 20:59:54 -0500

    fix: :bug: import Form component from the correct path in contactForm.tsx

--- src/components/contactForm.tsx
+++ src/components/contactForm.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 'use client'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Form, useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -12,6 +12,7 @@ import {
   FieldError,
   FieldLabel,
 } from '@/components/ui/field'
+import { Form } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
 
